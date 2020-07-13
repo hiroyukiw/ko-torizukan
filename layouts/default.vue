e6c80505ddf93a0f275b65c42e4e9f0ad850ff11
hiroyukiw <delta1887rai@gmail.com>
2020-07-14 07:05:12 +0900

    create_nuxt_project

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  v-app
+    v-main
+      v-container
+        nuxt
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  v-containar
+    v-row
+      v-col
+        .text-center
+          h1 Hello World
+</template>
+
+<script>
+export default {
+}
+</script>
